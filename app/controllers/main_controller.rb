6fa3068ba2d9f3209e6c168a214b59310bd4da17
Reymond Yncierto <rpyncierto@up.edu.ph>
2024-11-21 10:00:57 +0800

    feat: MAKDE user global

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,11 +2,5 @@ class MainController < ApplicationController
   def index
     # flash[:notice] = "Hello, Rails!"
     # flash[:alert] = "Goodbye, Rails!"
-
-    if session[:user_id]
-      # If the user is logged in than find the user
-      @user = User.find(session[:user_id])
-    end
-
   end
 end
